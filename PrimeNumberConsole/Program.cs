3d196976d0c02cc20658faf60f22d0e1f0240eef
andrei nfs <andreinfs@yahoo.com>
2018-11-30 11:57:57 +0200

    IntitialCommit

--- PrimeNumberConsole/Program.cs
+++ PrimeNumberConsole/Program.cs
@@ -1,12 +1,61 @@
 ﻿using System;
 
-namespace PrimeNumberConsole
+namespace Nr_prim
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int num1, k = 0;
+            do
+            {
+                Console.WriteLine("Insert a number:");
+                try
+                {
+                    num1 = Convert.ToInt32(Console.ReadLine());
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Exceptie-" + e);
+                    num1 = 0;
+                }
+            }
+            while (num1 <= 0);
+
+            if (num1 != 1)
+            {
+                for (int a = 2; a <= num1 / 2; a++)
+                {
+                    if (num1 % a == 0)
+                        k++;
+                }
+                if (k != 0)
+                {
+                    Console.BackgroundColor = ConsoleColor.DarkRed;
+                    Console.Clear();
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.WriteLine("Press any key to continue");
+                    Console.ReadKey();
+                }
+                else
+                {
+                    Console.BackgroundColor = ConsoleColor.DarkGreen;
+                    Console.Clear();
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.WriteLine("Press any key to continue");
+                    Console.ReadKey();
+                }
+            }
+            else if (num1 == 1)
+            {
+                Console.BackgroundColor = ConsoleColor.DarkRed;
+                Console.Clear();
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine("Press any key to continue");
+                Console.ReadKey();
+            }
+
+
         }
     }
 }
